1062e012a35f6d71b102d009d36d66ac9aca35ae
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-07-25 17:06:44 -0500

    36 - Miscellaneous, excluding pages, and redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import node from "@astrojs/node";
+
 
 // https://astro.build/config
 export default defineConfig({
-  // output: "hybrid",
-  // adapter: node({
-  //   mode: "standalone"
-  // })
-});
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+} // output: "hybrid",
+// adapter: node({
+//   mode: "standalone"
+// })
+);
